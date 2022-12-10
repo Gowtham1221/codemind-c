4b788ac7704ca28743cc40970a986f28df55fd95
Gowtham1221 <119719195+Gowtham1221@users.noreply.github.com>
2022-12-10 09:51:29 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,s=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);         s=s+a[i];     }     printf("%d",s); }

---
+++ Sum_of_Array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,s=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        s=s+a[i];
+    }
+    printf("%d",s);
+}
