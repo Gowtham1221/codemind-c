ad73bc2c46281b9b3cb02908e958c988e449749d
Gowtham1221 <119719195+Gowtham1221@users.noreply.github.com>
2022-12-29 10:47:12 +0530

    #include<stdio.h> int main() {     int n,i,a,b,max,s=0;     scanf("%d",&n);     int ar[n];     for(i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     scanf("%d%d",&a,&b);     max=a;     for(i=0;i<n;i++)     {        if(ar[i]>b||ar[i]<a)        {            s++;            if(ar[i]>max)            {                max=ar[i];            }        }     }     if(s==0)     {         printf("-1");     }     else     {     printf("%d",max);     } }

---
+++ maximum_of_numbers_not_between_A_and_B.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a,b,max,s=0;
+    scanf("%d",&n);
+    int ar[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    scanf("%d%d",&a,&b);
+    max=a;
+    for(i=0;i<n;i++)
+    {
+       if(ar[i]>b||ar[i]<a)
+       {
+           s++;
+           if(ar[i]>max)
+           {
+               max=ar[i];
+           }
+       }
+    }
+    if(s==0)
+    {
+        printf("-1");
+    }
+    else
+    {
+    printf("%d",max);
+    }
+}
+   
