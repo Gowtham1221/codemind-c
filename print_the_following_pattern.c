021426ee39bdcc93b79b11640961caa30d74a809
Gowtham1221 <119719195+Gowtham1221@users.noreply.github.com>
2023-01-21 20:42:37 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=n;j>=1;j--)
            {
                printf("%d ",j);
            }
            printf("
    ");
        }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -5,9 +5,10 @@ int main()
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j++)
+        for(j=n;j>=1;j--)
+        
         {
-            printf("%d",j);
+            printf("%d ",j);
         }
         printf("
 ");
